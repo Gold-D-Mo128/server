70334c5350d9e66cf73551cb6cfe675fda305c21
Gold-D-Mo128 <53493413+Gold-D-Mo128@users.noreply.github.com>
2023-10-27 12:18:31 +0400

    no build command

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+app.use(express.json());
+
+const messagesRoute = require("./routes/messages");
+app.use("/messages", messagesRoute);
 
 app.listen(PORT, () => {
   console.log(`server is running on : http://localhost:${PORT}`);
-});
-
-app.use(express.json());
-
-app.get("/firstApi", (req, res) => {
-  res.status(200).send({
-    status: "success",
-    data: {
-      message: "congrats, this is your first GET api",
-    },
-  });
-});
-
-app.post("/Api/:id", (req, res) => {
-  const { id } = req.params;
-  const { message } = req.body;
-
-  if (!message) {
-    res.status(418).send({ message: "no message" });
-  }
-
-  res.send({
-    id: `is this your id? ${id}`,
-    message: `is this your message? ${message}`,
-  });
 });
---
+++ routes/messages.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(200).send({
+    status: "success",
+    data: {
+      message: "congrats, this is your first GET api",
+    },
+  });
+});
+
+router.post("/:id", (req, res) => {
+  const { id } = req.params;
+  const { message } = req.body;
+
+  if (!message) {
+    res.status(418).send({ message: "no message" });
+  }
+
+  res.send({
+    id: `is this your id? ${id}`,
+    message: `is this your message? ${message}`,
+  });
+});
+
+module.exports = router;
