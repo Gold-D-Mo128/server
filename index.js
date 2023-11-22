4a77fc3253b8bbedf5293c05a5e829f481f25724
Gold-D-Mo128 <53493413+Gold-D-Mo128@users.noreply.github.com>
2023-11-22 07:26:27 +0000

    fixed the limiter

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 import { connectDB } from "./connection.js";
 import express from "express";
 import { messagesRouter } from "./routes/messagesRoutes.js";
-import { rateLimit } from 'express-rate-limit'
+
 import cors from "cors";
 
 const app = express();
 const PORT = 8000;
 
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Use an external store for consistency across multiple server instances.
-})
 
 
 app.use(limiter)
 app.use(cors());
 app.use(express.json());
 app.use("/messages", messagesRouter);
+app.use('/messages', limiter)
 
 app.listen(PORT, () => {
   // database();
--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -1,6 +1,17 @@
 import express from "express";
 import { Message } from "../models/message.js";
+import { rateLimit } from 'express-rate-limit'
+
+
 export const messagesRouter = express.Router();
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Use an external store for consistency across multiple server instances.
+})
 
 messagesRouter.get("/", async (req, res) => {
   const result = await Message.find();
@@ -12,7 +23,7 @@ messagesRouter.get("/", async (req, res) => {
   });
 });
 
-messagesRouter.post("/", async (req, res) => {
+messagesRouter.post("/", limiter ,async (req, res) => {
   try {
     const body = req.body;
     console.log("body: ", body);
