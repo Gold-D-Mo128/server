3c19c5c1737a1c7ad9e78d4ae99fe702ed0d261b
Gold-D-Mo128 <53493413+Gold-D-Mo128@users.noreply.github.com>
2023-11-22 07:54:09 +0000

    fixed the limiter v4

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -6,7 +6,7 @@ import { rateLimit } from 'express-rate-limit'
 export const messagesRouter = express.Router();
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
+	windowMs: 60 * 60 * 1000, // 15 minutes
 	limit: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 	standardHeaders: true, // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
